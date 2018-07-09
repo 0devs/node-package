87aaf0d17848754bf58b48094dc2c17da960e9d9
alekzonder <alekzonder@gmail.com>
2018-07-09 20:10:56 +0700

    init

---
+++ eslint.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "extends": "airbnb-base",
+    // "rules": {
+    //     "no-underscore-dangle": "off",
+    //     "class-methods-use-this": "off",
+    //     "no-param-reassign": "off"
+    //     "prefer-destructuring": "off"
+    // },
+    "env": {
+        "jest/globals": true
+    },
+    "plugins": ["jest"]
+};
