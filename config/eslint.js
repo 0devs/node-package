7391f4f0987f96bf25488275303a4242b20e5bd4
alekzonder <alekzonder@gmail.com>
2018-07-26 17:52:08 +0700

    chg: ignore some eslint rules

--- config/eslint.js
+++ config/eslint.js
@@ -2,7 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "no-underscore-dangle": "off",
-    //     "class-methods-use-this": "off",
+        "class-methods-use-this": "off",
     //     "no-param-reassign": "off"
     //     "prefer-destructuring": "off"
     },
