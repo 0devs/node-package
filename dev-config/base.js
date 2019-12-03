628ae90ee2c2ced49eaff8dd837c3e535758cee0
alekzonder <alekzonder@gmail.com>
2019-12-03 20:51:50 +0700

    chg: add jest to base deps

--- dev-config/base.js
+++ dev-config/base.js
@@ -1,6 +1,7 @@
 module.exports = {
   devDependencies: {
     eslint: '^6.7.2',
+    jest: '^24.9.0',
     'eslint-config-airbnb-base': '^14.0.0',
     'eslint-plugin-import': '^2.18.2',
     'eslint-plugin-jest': '^23.1.1',
