01fde668e2ac60b935b01d19f4fc21291f6e10a3
alekzonder <alekzonder@gmail.com>
2019-12-03 19:37:25 +0700

    chg: plugin-jest fixes

--- config/eslint-js.js
+++ config/eslint-js.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: [
     'airbnb-base',
     'plugin:sonarjs/recommended',
+    'plugin:jest/recommended',
     // 'plugin:security/recommended',
   ],
   rules: {
--- config/eslint-ts.js
+++ config/eslint-ts.js
@@ -5,19 +5,41 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:sonarjs/recommended',
+    // 'plugin:jest/recommended',
     // 'plugin:security/recommended',
   ],
   rules: {
     'no-underscore-dangle': 'off',
     'class-methods-use-this': 'off',
     'prefer-destructuring': 'off',
+    '@typescript-eslint/interface-name-prefix': ['error', { prefixWithI: 'always' }],
   },
   env: {
     'jest/globals': true,
   },
   plugins: [
-    'jest',
     'sonarjs',
     // 'security',
+  ],
+  overrides: [
+    {
+      files: [
+        '**/*.spec.ts',
+      ],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest'],
+      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+      // "extends": ["plugin:jest/recommended"]
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+        'jest/valid-expect-in-promise': 'error',
+      },
+    },
   ],
 };
